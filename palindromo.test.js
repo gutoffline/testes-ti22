95bc5b55fb99fcae74079a42a25a865c96c23608
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-06-18 20:27:32 -0300

    Palíndromo teste 'ônibus' true

--- palindromo.test.js
+++ palindromo.test.js
@@ -3,3 +3,11 @@ const palin = require ('./palindromo')
 test (' palindromo arara para true ', () =>  {
     expect (palin("arara")).toBe(true);
 })
+
+test (" palindromo 'Subi no ônibus' para true ", () =>  {
+    expect (palin("Subi no ônibus")).toBe(true);
+})
+
+test (" palindromo 'Marrocos' para true ", () =>  {
+    expect (palin("Marrocos")).toBe(false);
+})
