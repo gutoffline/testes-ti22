b6ec6789b7e6c260a19bf9dad0bff80e5d756f84
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-17 22:13:06 -0300

    test: soma 8 + 2 para dar 10

--- soma.test.js
+++ soma.test.js
@@ -8,3 +8,7 @@ test('soma 1 + 2 para dar 3', () => {
 test('soma 5 + 5 para dar 10', () => {
   expect(soma(5, 5)).toBe(10);
 });
+
+test('soma 8 + 2 para dar 10', () => {
+  expect(soma(8, 2)).toBe(10);
+});
