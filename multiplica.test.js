f4c0a873390bf10169315052d9ce8ebec6403da3
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-18 19:34:51 -0300

    test: multiplica -1 * -1 para dar 1

--- multiplica.test.js
+++ multiplica.test.js
@@ -3,3 +3,7 @@ const multiplica = require('./multiplica');
 test('multiplica 3 * 4 para dar 12', () => {
     expect(multiplica(3, 4)).toBe(12);
   });
+
+  test('multiplica -1 * -1 para dar 1', () => {
+    expect(multiplica(-1, -1)).toBe(1);
+  });
