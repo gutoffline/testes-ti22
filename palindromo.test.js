9338f10717afde9809ab87cc4f43b2a4990fb3d4
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-18 20:13:34 -0300

    palindromo "Marrocos" para dar "false

--- palindromo.test.js
+++ palindromo.test.js
@@ -8,3 +8,7 @@ test('palindromo "arara" para dar "true"', () => {
 test('palindromo "Subi no ônibus" para dar "true"', () => {
     expect(palindromo("Subi no ônibus")).toBe(true);
   });
+
+  test('palindromo "Marrocos" para dar "false"', () => {
+    expect(palindromo("Marrocos")).toBe(false);
+  });
