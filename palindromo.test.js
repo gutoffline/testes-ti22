c03a2cda64da4c6b409b57e3da69d714673f5ed1
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-18 20:12:27 -0300

    palindromo "Subi no ônibus" para dar "true

--- palindromo.test.js
+++ palindromo.test.js
@@ -5,3 +5,6 @@ test('palindromo "arara" para dar "true"', () => {
   expect(palindromo("Arara")).toBe(true);
 });
 
+test('palindromo "Subi no ônibus" para dar "true"', () => {
+    expect(palindromo("Subi no ônibus")).toBe(true);
+  });
