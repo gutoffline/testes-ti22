87bf70abf363ded25511b8d794e63547621e04a9
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-18 19:59:15 -0300

    test: palindromo "Subi no ônibus" para dar "true"

--- palindromo.test.js
+++ palindromo.test.js
@@ -3,3 +3,8 @@ const palindromo = require('./palindromo');
 test('palindromo "Arara" para dar "true"', () => {
     expect(palindromo("Arara")).toBe(true);
   });
+
+  test('palindromo "Subi no ônibus" para dar "true"', () => {
+    expect(palindromo("Subi no ônibus")).toBe(true);
+  });
+  
