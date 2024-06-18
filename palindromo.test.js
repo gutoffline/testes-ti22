17533beaff54b203bd1dadce47956cb52f6d9cc0
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-06-18 20:14:44 -0300

    test e alterações
    test 3
    alterações nas aspas

--- palindromo.test.js
+++ palindromo.test.js
@@ -1,11 +1,14 @@
 const palin = require('./palindromo');
 
 
-test('palindromo Arara para true',()=>{
+test("palindromo 'Arara' para true",()=>{
     expect( palin("Arara")).toBe(true);
 });
 
-test('palindromo Subi no ônibus para true', ()=>{
+test("palindromo 'Subi no ônibus' para true", ()=>{
     expect(palin("Subi no ônibus")).toBe(true);
 })
 
+test("palindromo 'Marrocos' para false", ()=>{
+    expect(palin("Marrocos")).toBe(false);
+})
