60d932820080180d310ea66819dc20d500be489c
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-18 19:36:03 -0300

    test: multiplica 0 * 5 para dar 0

--- multiplica.test.js
+++ multiplica.test.js
@@ -7,3 +7,7 @@ test('multiplica 3 * 4 para dar 12', () => {
   test('multiplica -1 * -1 para dar 1', () => {
     expect(multiplica(-1, -1)).toBe(1);
   });
+
+  test('multiplica 0 * 5 para dar 0', () => {
+    expect(multiplica(0, 5)).toBe(0);
+  });
