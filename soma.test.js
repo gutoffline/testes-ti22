d3ddcf9ba0e8140a2b390f9a2f5e8fdd774c0ceb
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-06-17 22:09:55 -0300

    teste soma de 5 + 5 = 10

--- soma.test.js
+++ soma.test.js
@@ -2,5 +2,10 @@ const soma = require('./soma');
 
 test('soma 1 + 2 para dar 3', () => {
   expect(soma(1, 2)).toBe(3);
-});
+}); 
+
+test('soma 5 + 5 para dar 10', () => {
+  expect(soma(5,5)).toBe(10);
+})
+
 
