b4a3684ba93cb675d53ca45fd63eddcf83e658ac
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-06-18 20:14:26 -0300

    Commits de divisão atrasados
    Divisão teste 10 / 2 = 5
    Divisão teste 9 / 3 = 3
    Divisão teste 1 / 0 = toThrow('Divisão por zero não é permitida')

--- divide.test.js
+++ divide.test.js
@@ -1,3 +1,11 @@
 const divide = require('./divide'); 
 
-test (' 10 / 2 para dar  ')
+test (' 10 / 2 para dar 5', () => {
+    expect (divide(10,2)).toBe(5);
+}); 
+test (' 9 / 3 para dar 3', () => {
+    expect (divide(9,3)).toBe(3);
+}); 
+test ('', () => {
+    expect (() => divide(1,0)).toThrow('Divisão por zero não é permitida');
+}); 
