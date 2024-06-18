0fdce4d5d78ef5edbb718245c28e42a340fd830c
Shadowind201 <enzocarnier@gmail.com>
2024-06-18 19:36:50 -0300

    subtrai 2

--- subtrai.test.js
+++ subtrai.test.js
@@ -5,5 +5,9 @@ test('subtrai 5 - 2 para dar 3', ()=> {
 });
 
 test('subtrai 0 - 0 para dar 0', ()=> {
-    expect(subtrai(0, 0)).toBe(0)
-})
+    expect(subtrai(0, 0)).toBe(0);
+});
+
+test('subtrai -2 - -2 para dar 0', ()=> {
+    expect(subtrai(-2, -2)).toBe(0);
+});
