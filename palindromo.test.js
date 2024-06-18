5240cf14a2e5eb3a03565e33c0854288877079da
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-18 20:03:47 -0300

    palindrome da para Subi no ônibuS

--- palindromo.test.js
+++ palindromo.test.js
@@ -3,3 +3,7 @@ const isPalindrome = require('./palindromo');
 test ('Palindrome da palavra Arara',()=>{
     expect (isPalindrome('arara')).toBe(true);
 });
+
+test ('Palindrome da palavra Subi no ônibus',()=>{
+    expect (isPalindrome('Subi no ônibus')).toBe(true);
+});
