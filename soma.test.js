e5de2d0d7ccc0394a1ce8202f2782126b237a963
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-18 19:24:18 -0300

    soma (-1) + (-1) para dar -2

--- soma.test.js
+++ soma.test.js
@@ -13,3 +13,7 @@ test('Soma 5 + 5 para dar 10',() => {
 test('Soma 8 + 2 para dar 10',() => {
   expect(soma(8,2)).toBe(10);
 })
+
+test('Soma (-1) + (-1) para dar -2',() =>{
+  expect(soma(-1,-1)).toBe(-2);
+})
