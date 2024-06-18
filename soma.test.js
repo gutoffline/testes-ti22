eb0cfc1656076590aee377a2cb8015a55c082260
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-06-17 22:08:43 -0300

    test: soma de 5+5 para dar 10

--- soma.test.js
+++ soma.test.js
@@ -4,3 +4,11 @@ test('soma 1 + 2 para dar 3', () => {
   expect(soma(1, 2)).toBe(3);
 });
 
+
+test('soma 5 + 5 para dar 10',()=>{
+  expect(soma(5, 5)).toBe(10);
+})
+
+test('subtrair 5 - 3 para dar 2', ()=>{
+  
+})
