7f83da3a63d833bfdd56921b52dc5ade316de39c
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-18 19:24:45 -0300

    test
    soma -1 + -1 para dar -2

--- soma.test.js
+++ soma.test.js
@@ -12,3 +12,7 @@ test('soma 5 + 5 para dar 10', () => {
 test('soma 8 + 2 para dar 10', () => {
   expect(soma(8, 2)).toBe(10);
 });
+
+test('soma -1 + -1 para dar -2', () => {
+  expect(soma(-1, -1)).toBe(-2);
+});
