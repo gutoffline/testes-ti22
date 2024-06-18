20e74617bf807ac9a649643f2dc3e78ebe9d7a33
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-06-18 19:46:33 -0300

    Multiplicação teste -1 x -1 = -2

--- multi.test.js
+++ multi.test.js
@@ -3,4 +3,11 @@ const multiplica = require('./multiplica');
 test (' multiplição 3 x 4 para dar 12 ', () => {
     expect(multiplica (3, 4)).toBe(12);
 
-} );
+} ); 
+
+const multiplica = require('./multiplica');
+
+test (' multiplicação -1 x -1 para dar 1 ', () => {
+    expect (multiplica (-1,-1)).toBe(-2);
+
+});
