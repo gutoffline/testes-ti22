11ff9311d9c7b3fbd95d34371d9202612f3bb378
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-06-18 19:46:51 -0300

    test
    multiplicação test 2

--- divide.test.js
+++ divide.test.js
@@ -3,3 +3,8 @@ const div = require('./divide');
 test('dividir 10 / 2 para dar 5', ()=>{
     expect(div(10,2)).toBe(5);
 })
+
+test('dividir 9 / 3 para dar 3', ()=>{
+    expect(div(9,3)).toBe(3);
+})
+
