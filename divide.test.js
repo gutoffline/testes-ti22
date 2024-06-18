b133d1f7b2bd71999f21a27ce556a62d5df20e09
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-18 19:58:21 -0300

    lança o erro ao dividir por zero

--- divide.test.js
+++ divide.test.js
@@ -8,3 +8,7 @@ test('divide 10 / 2  para dar 5', () => {
 test('divide 9 / 3 para dar 3', () => {
     expect(divide(9, 3)).toBe(3);
   });
+  
+  test('lança o erro ao dividir por zero', () => {
+    expect(() => divide(1, 0)).toThrow('Divisão por zero não é permitida');
+  });
