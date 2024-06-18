f403afabc47b4eb15c68366cc7da4c100e550212
Igor Santos <santosigor868@gmail.com>
2024-06-18 20:09:07 -0300

    test = Palindrome da palavra subi no onibus

--- palindromo.test.js
+++ palindromo.test.js
@@ -4,4 +4,10 @@ test('Palindrome da palavra Arara', () => {
     expect(isPalindromo('arara')).toBe(true);
   });
 
+  test('Palindrome da palavra subi no onibus', () => {
+    expect(isPalindromo('subi no onibus')).toBe(true);
+  });
+
+  
+
   
