45345e1bf15ddb02f3ae4fe93a41b251dfaa0a18
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-18 19:31:29 -0300

    test: subtrai -2 - -2 para dar 0

--- subtrai.test.js
+++ subtrai.test.js
@@ -7,3 +7,7 @@ test('subtrai 5 - 2 para dar 3', () => {
 test('subtrai 0 - 0 para dar 0', () => {
     expect(subtrai(0, 0)).toBe(0);
 });
+
+test('subtrai -2 - -2 para dar 0', () => {
+    expect(subtrai(-2, -2)).toBe(0);
+});
