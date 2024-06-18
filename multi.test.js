39ab6a15f97518e4f30ac85d8c33758a6bff0ca0
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-06-18 19:34:13 -0300

    Subtração teste  5 - 2 = 3

---
+++ multi.test.js
@@ -0,0 +1,2 @@
+const multiplica = require('./multiplica'); 
+
---
+++ subtracao_test.js
@@ -0,0 +1,13 @@
+const subtrai = require('./subtrai'); 
+
+test('subtrai 5 - 2 para dar 3', () => {
+    expect(subtrai (5, 2)).toBe(3);
+  });  
+   
+test ('subtrai 0 - 0 para dar 0', () => {
+    expect(subtrai (0, 0)).toBe(0);
+}); 
+
+test ('subtrai -2 - -2 para dar 0', () => {
+    expect(subtrai (-2, -2)).toBe(0);
+});
