079cfdf6bd04c52f90fd8232731268c108d473ec
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-18 19:55:26 -0300

    Divide 1 / 0 para dar erro de não permitido

--- divide.test.js
+++ divide.test.js
@@ -8,3 +8,6 @@ test ('Divide 9 / 3 para dar 3',()=>{
     expect (divide(9,3)).toBe(3);
 });
 
+test ('lança erro ao dividir por zero', () =>{
+    expect(() => divide(1,0)).toThrow('Divisão por zero não é permitida');
+});
