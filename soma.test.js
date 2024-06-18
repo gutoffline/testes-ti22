6567f5edb6b88cb76c3ebd98ec4393c08f1ccfac
GUILHERME PEREIRA <guilhermemap85@gmail.com>
2024-06-17 22:14:11 -0300

    Test: soma de 8 + 2 para dar 10

--- soma.test.js
+++ soma.test.js
@@ -1,4 +1,5 @@
 const soma = require('./soma');
+const subtrai = require('./subtrai');
 
 test('soma 1 + 2 para dar 3', () => {
   expect(soma(1, 2)).toBe(3);
@@ -9,6 +10,6 @@ test('soma 5 + 5 para dar 10',()=>{
   expect(soma(5, 5)).toBe(10);
 })
 
-test('subtrair 5 - 3 para dar 2', ()=>{
-  
+test('soma 8 + 2 para dar 10', ()=>{
+  expect(soma(8, 2)).toBe(10);
 })
