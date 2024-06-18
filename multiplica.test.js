50256b6fa88c15b6d9d90a23b5db00cbfaa398cd
Igor Santos <santosigor868@gmail.com>
2024-06-18 19:45:30 -0300

    test = multiplica 0 * 5 para dar 0

--- multiplica.test.js
+++ multiplica.test.js
@@ -3,3 +3,11 @@ const multiplica = require('./multiplica');
 test('multiplica 3 * 4 para dar 12', () => {
     expect(multiplica(3, 4)).toBe(12);
   });
+
+  test('multiplica -1 * -1 para dar 12', () => {
+    expect(multiplica(-1, -1)).toBe(1);
+  });
+
+  test('multiplica 0 * 5 para dar 0', () => {
+    expect(multiplica(0, 5)).toBe(0);
+  });
