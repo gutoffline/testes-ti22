5e3851c6a430e3d480dd269d92e558d6d6ea7f35
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-18 20:00:13 -0300

    test: palindromo "Marrocos" para dar "false"

--- palindromo.test.js
+++ palindromo.test.js
@@ -7,4 +7,8 @@ test('palindromo "Arara" para dar "true"', () => {
   test('palindromo "Subi no ônibus" para dar "true"', () => {
     expect(palindromo("Subi no ônibus")).toBe(true);
   });
+
+  test('palindromo "Marrocos" para dar "false"', () => {
+    expect(palindromo("Marrocos")).toBe(false);
+  });
   
