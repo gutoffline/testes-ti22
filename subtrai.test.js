d68f3a8080f10f795ae87fa8275d1fe645dd994b
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-18 19:34:25 -0300

    test subtrai
    subtrai 0 - 0 para dar 0

--- subtrai.test.js
+++ subtrai.test.js
@@ -4,3 +4,7 @@ const subtrai = require('./subtrai');
 test('subtrai 5 - 2 para dar 3', () => {
   expect(subtrai(5, 2)).toBe(3);
 });
+
+test('subtrai 0 - 0 para dar 0', () => {
+    expect(subtrai(0, 0)).toBe(0);
+  });
