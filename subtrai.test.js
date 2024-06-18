a5e9defe8a07dd27d0b0650adf4933f990f6099a
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-18 19:29:36 -0300

    subtrai 0 - 0 para dar 0

--- subtrai.test.js
+++ subtrai.test.js
@@ -3,3 +3,8 @@ const subtrai = require('./subtrai');
 test ('Subtrai 5 - 2 para dar 3',() =>{
     expect(subtrai(5,2)).toBe(3);
 });
+
+test ('Subtrai 0 - 0 para dar 0',() =>{
+    expect(subtrai(0,0)).toBe(0);
+});
+
